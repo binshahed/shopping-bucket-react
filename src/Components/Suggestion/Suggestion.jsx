ef309cfaaf7e9e0f103ed85e015fa75161d3fd74
Mohammad Bin Shahed <mdbinshahed5@gmail.com>
2021-05-04 17:08:29 +0600

    search overly add

--- src/Components/Suggestion/Suggestion.jsx
+++ src/Components/Suggestion/Suggestion.jsx
@@ -1,47 +1,44 @@
+import useShoppingItems from "../../app/hooks/useShoppingItem";
 import leftArrowIcon from "../../assets/icons/left-arrow-icon.png";
 import rightArrowIcon from "../../assets/icons/right-arrow-icon.png";
 
 const Suggestion = () => {
-    return (
-        <div className="add-item__sugestions">
-        <div className="add-item__sugestions-control add-item__sugestions-control--prev">
-          <img
-            src={leftArrowIcon}
-            alt="Previous"
-            className="add-item__sugestions-control-icon"
-          />
-        </div>
-        <div className="add-item-sugestions-chips">
-          <div className="add-item__sugestions-chip-item">Eggs</div>
-          <div className="add-item__sugestions-chip-item">Milk</div>
-          <div className="add-item__sugestions-chip-item">Apple</div>
-          <div className="add-item__sugestions-chip-item">coke</div>
-          <div className="add-item__sugestions-chip-item">Water</div>
-          <div className="add-item__sugestions-chip-item">Apple</div>
-          <div className="add-item__sugestions-chip-item">Orange</div>
-          <div className="add-item__sugestions-chip-item">Banana</div>
-          <div className="add-item__sugestions-chip-item">Fish</div>
-          <div className="add-item__sugestions-chip-item">Carrot</div>
-          <div className="add-item__sugestions-chip-item">Computer</div>
-          <div className="add-item__sugestions-chip-item">Mouse</div>
-          <div className="add-item__sugestions-chip-item">Key board</div>
-          <div className="add-item__sugestions-chip-item">Laptop</div>
-          <div className="add-item__sugestions-chip-item">Monitor</div>
-          <div className="add-item__sugestions-chip-item">Book</div>
-          <div className="add-item__sugestions-chip-item">Pen</div>
-          <div className="add-item__sugestions-chip-item">Mobile</div>
-          <div className="add-item__sugestions-chip-item">Table</div>
-          <div className="add-item__sugestions-chip-item">Chair</div>
-        </div>
-        <div className="add-item__sugestions-control add-item__sugestions-control--next">
-          <img
-            src={rightArrowIcon}
-            alt="Previous"
-            className="add-item__sugestions-control-icon"
-          />
-        </div>
+  const { createItem, getSuggestions } = useShoppingItems;
+  const handleClick = (sug) => {
+    createItem(sug.text, sug);
+  };
+  const suggestions = getSuggestions;
+
+  return (
+    <div className="add-item__sugestions">
+      <div className="add-item__sugestions-control add-item__sugestions-control--prev">
+        <img
+          src={leftArrowIcon}
+          alt="Previous"
+          className="add-item__sugestions-control-icon"
+        />
       </div>
-    );
+      <div className="add-item-sugestions-chips">
+        {suggestions.length > 0 &&
+          suggestions.map((sug) => {
+            <div
+              key={sug.id}
+              className="add-item__sugestions-chip-item"
+              onClick={() => handleClick(sug)}
+            >
+              {sug.text}
+            </div>;
+          })}
+      </div>
+      <div className="add-item__sugestions-control add-item__sugestions-control--next">
+        <img
+          src={rightArrowIcon}
+          alt="Previous"
+          className="add-item__sugestions-control-icon"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Suggestion;
